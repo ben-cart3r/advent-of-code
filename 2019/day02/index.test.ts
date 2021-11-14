a01a8a611e65fa32653f7da90c3d4bad606e05da
Ben Carter <benjamin.carter93@outlook.com>
2021-11-14 09:32:50 +0000

    Skip day 2 tests

--- 2019/day02/index.test.ts
+++ 2019/day02/index.test.ts
@@ -2,7 +2,7 @@ import { part1, part2 } from ".";
 
 const sampleData = "1,1,1,4,99,5,6,0,99";
 
-describe("2019-02", () => {
+describe.skip("2019-02", () => {
     describe("part1", () => {
         it("should solve for sample data", () => {
             const result = part1(sampleData);
