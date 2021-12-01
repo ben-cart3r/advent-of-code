b19704307ee081ccf56d1686ba8e0ad9a3c5d349
Ben Carter <benjamin.carter93@outlook.com>
2021-12-01 09:07:28 +0000

    Remove sample 2 from tests

--- 2021/day01/index.test.ts
+++ 2021/day01/index.test.ts
@@ -11,17 +11,6 @@ const sampleData = `199
 260
 263`;
 
-const sampleData2 = `199  A
-200  A B
-208  A B C
-210    B C D
-200  E   C D
-207  E F   D
-240  E F G
-269    F G H
-260      G H
-263        H`;
-
 describe("2021-01", () => {
     describe("part1", () => {
         it("should solve for sample data", () => {
