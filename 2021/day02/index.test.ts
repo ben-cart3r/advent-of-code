58de33ec8c3893f704764adcb54221fad23205c1
Ben Carter <benjamin.carter93@outlook.com>
2021-12-02 08:44:50 +0000

    Part 1 and 2

---
+++ 2021/day02/index.test.ts
@@ -0,0 +1,26 @@
+import { part1, part2 } from ".";
+
+const sampleData = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`;
+
+describe("year-day", () => {
+    describe("part1", () => {
+        it("should solve for sample data", () => {
+            const result = part1(sampleData);
+
+            expect(result).toEqual("150");
+        });
+    });
+
+    describe("part2", () => {
+        it("should solve for sample data", () => {
+            const result = part2(sampleData);
+
+            expect(result).toEqual("900");
+        });
+    });
+});
---
+++ 2021/day02/index.ts
@@ -0,0 +1,65 @@
+const parse = (input: string) => {
+    return input.split("\n").map((line) => {
+        const [dir, len] = line.split(" ");
+
+        return {
+            dir,
+            len: parseInt(len),
+        };
+    });
+};
+
+export const part1 = (input: string) => {
+    const commands = parse(input);
+
+    const [horizontal, depth] = commands.reduce(
+        ([h, d], command) => {
+            const { dir, len } = command;
+
+            switch (dir) {
+                case "forward":
+                    h += len;
+                    break;
+                case "up":
+                    d -= len;
+                    break;
+                case "down":
+                    d += len;
+                    break;
+            }
+
+            return [h, d];
+        },
+        [0, 0]
+    );
+
+    return (horizontal * depth).toString();
+};
+
+export const part2 = (input: string) => {
+    const commands = parse(input);
+
+    const [horizontal, depth] = commands.reduce(
+        ([h, d, a], command) => {
+            const { dir, len } = command;
+
+            switch (dir) {
+                case "forward":
+                    h += len;
+                    d += a * len;
+                    break;
+                case "up":
+                    a -= len;
+                    break;
+                case "down":
+                    a += len;
+                    break;
+            }
+
+            return [h, d, a];
+        },
+        [0, 0, 0]
+    );
+
+    return (horizontal * depth).toString();
+};
