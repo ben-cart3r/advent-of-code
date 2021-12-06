3a40369597b410a3d5879ae95fedd735ff2a0749
Ben Carter <benjamin.carter93@outlook.com>
2021-12-06 13:02:18 +0000

    Part 1 and 2

---
+++ 2021/day06/index.test.ts
@@ -0,0 +1,21 @@
+import { part1, part2 } from ".";
+
+const sampleData = "3,4,3,1,2";
+
+describe("year-day", () => {
+    describe("part1", () => {
+        it("should solve for sample data", () => {
+            const result = part1(sampleData);
+
+            expect(result).toEqual("5934");
+        });
+    });
+
+    describe("part2", () => {
+        it("should solve for sample data", () => {
+            const result = part2(sampleData);
+
+            expect(result).toEqual("26984457539");
+        });
+    });
+});
