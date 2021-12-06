d61f820f62eb8b384e2e5b673436c47172f84d90
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2021-12-06 13:04:26 +0000

    Year: 2021, Day: 06, Parts: 1, 2 (#8)

---
+++ 2021/day06/index.ts
@@ -0,0 +1,43 @@
+import { sum } from "../../common";
+
+const parse = (input: string) => {
+    return input.split(",").map((item) => parseInt(item));
+};
+
+const simulate = (fishes: Array<number>, days: number) => {
+    // Count fishes by days until reproduction
+    const gestationPeriods = fishes.reduce(
+        (acc, fish) => {
+            acc[fish] += 1;
+            return acc;
+        },
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    );
+
+    for (let i = 0; i < days; ++i) {
+        // Pop from front of array
+        const newFishes = gestationPeriods.shift();
+
+        // Reset fishes that reproduced
+        gestationPeriods[6] += newFishes;
+
+        // Create new fishes
+        gestationPeriods.push(newFishes);
+    }
+
+    return sum(...gestationPeriods);
+};
+
+export const part1 = (input: string): string => {
+    const timers = parse(input);
+    const fishCount = simulate(timers, 80);
+
+    return fishCount.toString();
+};
+
+export const part2 = (input: string): string => {
+    const timers = parse(input);
+    const fishCount = simulate(timers, 256);
+
+    return fishCount.toString();
+};
