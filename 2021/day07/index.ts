adea7a842b0dd5c371dc4a498cf021a0257a3e94
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2021-12-07 09:07:35 +0000

    Year: 2021, Day: 07, Parts: 1, 2 (#10)

---
+++ 2021/day07/index.ts
@@ -0,0 +1,30 @@
+import { sum } from "../../common";
+
+const parse = (input: string) => {
+    return input
+        .trim()
+        .split(",")
+        .map((item) => parseInt(item));
+};
+
+const triangleNum = (n: number) => (n * (n + 1)) / 2;
+
+export const part1 = (input: string): string => {
+    const crabs = parse(input);
+
+    const scores = crabs.map((_, idx) =>
+        sum(...crabs.map((crab) => Math.abs(crab - idx)))
+    );
+
+    return Math.min(...scores).toString();
+};
+
+export const part2 = (input: string): string => {
+    const crabs = parse(input);
+
+    const scores = crabs.map((_, idx) =>
+        sum(...crabs.map((crab) => triangleNum(Math.abs(crab - idx))))
+    );
+
+    return Math.min(...scores).toString();
+};
