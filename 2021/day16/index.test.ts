11997a9f9bc15aaeb70ef55f3472a8f447a04f97
Ben Carter <benjamin.carter93@outlook.com>
2021-12-16 11:35:51 +0000

    Add tests

--- 2021/day16/index.test.ts
+++ 2021/day16/index.test.ts
@@ -1,21 +1,19 @@
 import { part1, part2 } from ".";
 
-const sampleData = "";
-
-describe("year-day", () => {
+describe("2021-16", () => {
     describe("part1", () => {
         it("should solve for sample data", () => {
-            const result = part1(sampleData);
+            const result = part1("A0016C880162017C3686B18A3D4780");
 
-            expect(result).toEqual("");
+            expect(result).toEqual("31");
         });
     });
 
     describe("part2", () => {
         it("should solve for sample data", () => {
-            const result = part2(sampleData);
+            const result = part2("9C0141080250320F1802104A08");
 
-            expect(result).toEqual("");
+            expect(result).toEqual("1");
         });
     });
 });
