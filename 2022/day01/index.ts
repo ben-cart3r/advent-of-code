ed358b06239768243f68e1be01fd72a0d4c163ca
Ben Carter <benjamin.carter93@outlook.com>
2022-12-01 22:35:42 +0000

    refactor 2022 day 01

--- 2022/day01/index.ts
+++ 2022/day01/index.ts
@@ -2,24 +2,41 @@ import { sum } from "../../common";
 import Input from "../../common/input";
 import { asc } from "../../common/sorting";
 
-const parse = (input: string) => {
-    return new Input(input).asParagraphs().asIntegers();
+type ElfFood = {
+    calories: number;
+};
+
+type Elf = {
+    inventory: {
+        food: Array<ElfFood>;
+    };
+};
+
+const parse = (input: string): Array<Elf> => {
+    return new Input(input)
+        .asParagraphs()
+        .asIntegers()
+        .map((calorieCounts) => ({
+            inventory: {
+                food: calorieCounts.map((calories) => ({ calories })),
+            },
+        }));
+};
+
+const calculateCalorieTotal = (elf: Elf): number => {
+    return sum(...elf.inventory.food.map((item) => item.calories));
 };
 
 export const part1 = (input: string): string => {
-    const elvesInventories = parse(input);
-    const calorieCounts = elvesInventories.map((inventory) =>
-        sum(...inventory)
-    );
+    const elves = parse(input);
+    const calorieCounts = elves.map(calculateCalorieTotal);
 
     return Math.max(...calorieCounts).toString();
 };
 
 export const part2 = (input: string): string => {
-    const elvesInventories = parse(input);
-    const calorieCounts = elvesInventories.map((inventory) =>
-        sum(...inventory)
-    );
+    const elves = parse(input);
+    const calorieCounts = elves.map(calculateCalorieTotal);
     const sorted = calorieCounts.sort(asc);
 
     return sum(...sorted.slice(0, 3)).toString();
