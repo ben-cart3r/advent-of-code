8041155f6547cdb375bccb5e8212dec3ae0b4188
Ben Carter <benjamin.carter93@outlook.com>
2022-12-01 06:35:27 +0000

    fmt

--- 2022/day01/index.ts
+++ 2022/day01/index.ts
@@ -7,14 +7,18 @@ const parse = (input: string) => {
 
 export const part1 = (input: string): string => {
     const elvesInventories = parse(input);
-    const calorieCounts = elvesInventories.map((inventory) => sum(...inventory));
+    const calorieCounts = elvesInventories.map((inventory) =>
+        sum(...inventory)
+    );
 
     return Math.max(...calorieCounts).toString();
 };
 
 export const part2 = (input: string): string => {
     const elvesInventories = parse(input);
-    const calorieCounts = elvesInventories.map((inventory) => sum(...inventory));
+    const calorieCounts = elvesInventories.map((inventory) =>
+        sum(...inventory)
+    );
     const sorted = calorieCounts.sort(asc);
 
     return sum(...sorted.slice(0, 3)).toString();
