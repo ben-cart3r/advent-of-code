b35c25ed825ef3a24e3de08c6d35c4950d0d79e8
Ben Carter <benjamin.carter93@outlook.com>
2022-12-13 23:05:20 +0000

    Day13

---
+++ 2022/day13/index.test.ts
@@ -0,0 +1,43 @@
+import { part1, part2 } from ".";
+
+const sampleData = `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`;
+
+describe("2022-13", () => {
+    describe("part1", () => {
+        it("should solve for sample data", () => {
+            const result = part1(sampleData);
+
+            expect(result).toEqual("13");
+        });
+    });
+
+    describe("part2", () => {
+        it("should solve for sample data", () => {
+            const result = part2(sampleData);
+
+            expect(result).toEqual("140");
+        });
+    });
+});
