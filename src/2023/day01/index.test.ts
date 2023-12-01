948ecc1a555b9f907d609463eee85839dcbe702c
Ben Carter <benjamin.carter93@outlook.com>
2023-12-01 07:59:03 +0000

    2023 Day 01

--- src/2023/day01/index.test.ts
+++ src/2023/day01/index.test.ts
@@ -1,21 +1,32 @@
 import { part1, part2 } from ".";
 
-const sampleData = "";
+const sampleData = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
+
+const sampleData2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
 
 describe("2023-01", () => {
     describe("part1", () => {
         it("should solve for sample data", () => {
             const result = part1(sampleData);
 
-            expect(result).toEqual("");
+            expect(result).toEqual("142");
         });
     });
 
     describe("part2", () => {
         it("should solve for sample data", () => {
-            const result = part2(sampleData);
+            const result = part2(sampleData2);
 
-            expect(result).toEqual("");
+            expect(result).toEqual("281");
         });
     });
 });
--- src/2023/day01/index.ts
+++ src/2023/day01/index.ts
@@ -1,16 +1,80 @@
-// import Input from "../../common/input";
+import { sum } from "../../common";
+import Input from "../../common/input";
 
-const parse = (input: string): string => {
-    // return new Input(input).asString();
-    return input;
+const digitTokens = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const stringTokens = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+];
+
+const tokenize = (input: string, validTokens: Array<string>) => {
+    const tokens: Array<string> = [];
+
+    for (let i = 0; i < input.length; ++i) {
+        for (const token of validTokens) {
+            if (input.substring(i).startsWith(token)) {
+                tokens.push(token);
+            }
+        }
+    }
+
+    return tokens;
 };
 
+const translate = (tokens: Array<string>) => {
+    const map: Record<string, string> = {
+        one: "1",
+        two: "2",
+        three: "3",
+        four: "4",
+        five: "5",
+        six: "6",
+        seven: "7",
+        eight: "8",
+        nine: "9",
+    };
+
+    return tokens.map((token) => {
+        if (map[token]) {
+            return map[token];
+        }
+        return token;
+    });
+};
+
+const parse = (
+    input: string,
+    replaceStringRepresentation: boolean
+): Array<Array<string>> => {
+    const lines = new Input(input).asLines().asStrings();
+    const tokens = replaceStringRepresentation
+        ? [...digitTokens, ...stringTokens]
+        : digitTokens;
+    const tokenised = lines.map((line) => tokenize(line, tokens));
+    const translated = tokenised.map((line) => translate(line));
+
+    return translated;
+};
+
+const getCalibrationValue = (line: Array<string>) =>
+    parseInt(line[0] + line[line.length - 1]);
+
 export const part1 = (input: string): string => {
-    const parsed = parse(input);
-    return parsed;
+    const lines = parse(input, false);
+    const values = lines.map(getCalibrationValue);
+    return sum(...values).toString();
 };
 
 export const part2 = (input: string): string => {
-    const parsed = parse(input);
-    return parsed;
+    const lines = parse(input, true);
+    const values = lines.map(getCalibrationValue);
+    return sum(...values).toString();
 };
