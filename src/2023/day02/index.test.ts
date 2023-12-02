7b71a5913f4caca9fad8189a4a41b33fcbe12812
Ben Carter <benjamin.carter93@outlook.com>
2023-12-02 07:16:09 +0000

    2023 Day 02

--- src/2023/day02/index.test.ts
+++ src/2023/day02/index.test.ts
@@ -1,13 +1,17 @@
 import { part1, part2 } from ".";
 
-const sampleData = "";
+const sampleData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
 describe("2023-02", () => {
     describe("part1", () => {
         it("should solve for sample data", () => {
             const result = part1(sampleData);
 
-            expect(result).toEqual("");
+            expect(result).toEqual("8");
         });
     });
 
@@ -15,7 +19,7 @@ describe("2023-02", () => {
         it("should solve for sample data", () => {
             const result = part2(sampleData);
 
-            expect(result).toEqual("");
+            expect(result).toEqual("2286");
         });
     });
 });
--- src/2023/day02/index.ts
+++ src/2023/day02/index.ts
@@ -1,16 +1,88 @@
-// import Input from "../../common/input";
+import { flatten, sum } from "../../common";
+import Input from "../../common/input";
 
-const parse = (input: string): string => {
-    // return new Input(input).asString();
-    return input;
+type CubeColour = "red" | "green" | "blue";
+
+type CubeGroup = {
+    colour: CubeColour;
+    count: number;
+};
+
+type Game = {
+    id: string;
+    turns: Array<CubeGroup>;
+};
+
+const parseCubeGroup = (input: string): CubeGroup => {
+    const [count, colour] = input.trim().split(" ");
+
+    return {
+        colour: colour as CubeColour,
+        count: parseInt(count),
+    };
+};
+
+const parseGameTurn = (input: string): Array<CubeGroup> => {
+    return input.split(",").map(parseCubeGroup);
+};
+
+const parseGame = (input: string): Game => {
+    const [id, turns] = input.split(":");
+    const shortId = id.substring(5);
+
+    return {
+        id: shortId,
+        turns: flatten(turns.split(";").map(parseGameTurn)),
+    };
+};
+
+const parse = (input: string) => {
+    return new Input(input).asLines().asStrings().map(parseGame);
+};
+
+const getMaximumDrawnCubes = (game: Game, color: CubeColour) => {
+    const occurrences = game.turns
+        .filter(({ colour }) => colour == color)
+        .map(({ count }) => count);
+    return Math.max(...occurrences);
+};
+
+const isValidGame = (
+    game: Game,
+    redThreshold: number,
+    greenThreshold: number,
+    blueThreshold: number
+) => {
+    const maxReds = getMaximumDrawnCubes(game, "red");
+    const maxGreens = getMaximumDrawnCubes(game, "green");
+    const maxBlues = getMaximumDrawnCubes(game, "blue");
+
+    return (
+        maxReds <= redThreshold &&
+        maxGreens <= greenThreshold &&
+        maxBlues <= blueThreshold
+    );
+};
+
+const getGameScore = (game: Game) => {
+    const maxReds = getMaximumDrawnCubes(game, "red");
+    const maxGreens = getMaximumDrawnCubes(game, "green");
+    const maxBlues = getMaximumDrawnCubes(game, "blue");
+
+    return maxReds * maxGreens * maxBlues;
 };
 
 export const part1 = (input: string): string => {
-    const parsed = parse(input);
-    return parsed;
+    const games = parse(input);
+    const validGames = games.filter((game) => isValidGame(game, 12, 13, 14));
+    const validGameIds = validGames.map(({ id }) => parseInt(id));
+
+    return sum(...validGameIds).toString();
 };
 
 export const part2 = (input: string): string => {
-    const parsed = parse(input);
-    return parsed;
+    const games = parse(input);
+    const scores = games.map(getGameScore);
+
+    return sum(...scores).toString();
 };
