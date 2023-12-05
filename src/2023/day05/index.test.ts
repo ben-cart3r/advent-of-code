bd7889445457acd2bca1477ab512403ec1c4a77e
Ben Carter <benjamin.carter93@outlook.com>
2023-12-05 09:48:49 +0000

    2023 Day 05 - WIP

--- src/2023/day05/index.test.ts
+++ src/2023/day05/index.test.ts
@@ -1,21 +1,53 @@
 import { part1, part2 } from ".";
 
-const sampleData = "";
+const sampleData = `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`;
 
 describe("2023-05", () => {
-    describe("part1", () => {
-        it("should solve for sample data", () => {
-            const result = part1(sampleData);
+  describe("part1", () => {
+    it("should solve for sample data", () => {
+      const result = part1(sampleData);
 
-            expect(result).toEqual("");
-        });
+      expect(result).toEqual("35");
     });
+  });
 
-    describe("part2", () => {
-        it("should solve for sample data", () => {
-            const result = part2(sampleData);
+  describe("part2", () => {
+    it("should solve for sample data", () => {
+      const result = part2(sampleData);
 
-            expect(result).toEqual("");
-        });
+      expect(result).toEqual("");
     });
+  });
 });
--- src/2023/day05/index.ts
+++ src/2023/day05/index.ts
@@ -1,16 +1,99 @@
-// import Input from "../../common/input";
+import { chunks, inRange } from "../../common";
+import { toNumber } from "../../common/string-helpers";
 
-const parse = (input: string): string => {
-    // return new Input(input).asString();
-    return input;
+const parseSeeds = (input: string) => {
+  const [, numbers] = input.split(":");
+
+  return numbers.trim().split(" ").map(toNumber);
+};
+
+const parseMap = (input: string) => {
+  const [header, ...maps] = input.split("\n");
+  const headerParts = header.split(" ").at(0)!.split("-");
+
+  return {
+    from: headerParts[0],
+    to: headerParts[2],
+    maps: maps.map(line => line.split(" ").map(toNumber)),
+  };
+};
+
+const parseMaps = (inputs: Array<string>) => {
+  return inputs.map(parseMap);
+};
+
+const parse = (input: string) => {
+  const [seeds, ...maps] = input.trim().split("\n\n");
+
+  return {
+    seeds: parseSeeds(seeds),
+    maps: parseMaps(maps),
+  };
 };
 
 export const part1 = (input: string): string => {
-    const parsed = parse(input);
-    return parsed;
+  const parsed = parse(input);
+
+  const values = parsed.seeds.map(seed => {
+    let type = "seed";
+    while (type != "location") {
+      let hasChanged = false;
+      const mapper = parsed.maps.find(map => map.from == type)!;
+
+      // console.log(`From: ${mapper.from}, To: ${mapper.to}`);
+      for (const [dest, source, range] of mapper.maps) {
+        // console.log(`From: ${source}, To: ${source + range}, value: ${seed}`);
+        if (inRange(seed, source, source + range) && !hasChanged) {
+          // inclusive or exclusive?
+          // console.log(`New Value: ${dest + (seed - source)}`);
+          seed = dest + (seed - source);
+          hasChanged = true;
+        }
+      }
+
+      type = mapper.to;
+    }
+    // console.log("");
+    return seed;
+  });
+
+  return Math.min(...values).toString();
 };
 
 export const part2 = (input: string): string => {
-    const parsed = parse(input);
-    return parsed;
+  const parsed = parse(input);
+  const ranges = chunks(parsed.seeds, 2);
+
+  const values = ranges.map(([min, range]) => {
+    console.log(`Seed Range: ${min} - ${min + range}`);
+    const seeds = Array.from({ length: range }, (_, i) => i + min);
+    console.log(`Created array`);
+    const v = seeds.map(seed => {
+      let type = "seed";
+      while (type != "location") {
+        let hasChanged = false;
+        const mapper = parsed.maps.find(map => map.from == type)!;
+
+        // console.log(`From: ${mapper.from}, To: ${mapper.to}`);
+        for (const [dest, source, range] of mapper.maps) {
+          // console.log(`From: ${source}, To: ${source + range}, value: ${seed}`);
+          if (inRange(seed, source, source + range) && !hasChanged) {
+            // inclusive or exclusive?
+            // console.log(`New Value: ${dest + (seed - source)}`);
+            seed = dest + (seed - source);
+            hasChanged = true;
+          }
+        }
+
+        type = mapper.to;
+      }
+
+      // console.log("");
+      return seed;
+    });
+
+    return Math.min(...v);
+  });
+
+  return Math.min(...values).toString();
 };
